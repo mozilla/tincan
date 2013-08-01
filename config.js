c9c4fcdbf01665881c504922677a8d76f732b363
Ryan Seys <ryan@ryanseys.com>
2013-08-01 11:01:15 -0700

    Reset to 127.0.0.1

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   domain: process.env.PUBLIC_URL || null,
-  host: process.env.HOST || "http://10.251.30.175",
+  host: process.env.HOST || "http://127.0.0.1",
   port: process.env.PORT || 3000,
   persona_uri: process.env.PERSONA_URI || "https://webrtc.personatest.org",
   persona_verifier_uri: process.env.PERSONA_VERIFIER_URI || "https://webrtc.personatest.org/verify",
