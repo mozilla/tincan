d42da74d89ddd77f4c7e3de87bb5a2629421afa5
Ryan Seys <ryan@ryanseys.com>
2013-08-20 12:40:28 -0700

    Add set idp provider logic

--- config.js
+++ config.js
@@ -4,6 +4,9 @@ module.exports = {
   port: process.env.PORT || 3000,
   persona_uri: process.env.PERSONA_URI || "https://webrtc.personatest.org",
   persona_verifier_uri: process.env.PERSONA_VERIFIER_URI || "https://webrtc.personatest.org/verify",
+  idp_provider: 'https://webrtc.personatest.org',
+  idp_protocol: 'browserid',
+  idp_username: null,
   title: process.env.TINCAN_TITLE || "Tin Can",
   secretsauce: process.env.SECRETSAUCE || "super secret sauce"
 };
--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,15 @@ exports.index = function(req, res){
     res.render('index', { title: config.title, persona_uri: uri });
   }
   else {
-    res.render('call', { title: config.title, persona_uri: uri, email: email, server: config.domain || config.host + ":" + config.port });
+    res.render('call', {
+      title: config.title,
+      persona_uri: uri,
+      email: email,
+      server: config.domain || config.host + ":" + config.port,
+      idp_provider: config.idp_provider,
+      idp_protocol: config.idp_protocol,
+      idp_username: config.idp_username
+    });
   }
 };
 
